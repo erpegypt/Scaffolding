e1d0b43c7c2df3a863b1509af105d929a95ff3d8
Connect4 <connect4system@gmail.com>
2023-10-19 12:35:36 +0300

    first
    scaffolding

---
+++ scaffolding/config/docs.py
@@ -0,0 +1,10 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/erpegypt/scaffolding"
+# headline = "App for scaffolding and equipment rent operation managment"
+# sub_heading = "rent and sales return and invoice "
+
+def get_context(context):
+	context.brand_html = "Scaffolding"
